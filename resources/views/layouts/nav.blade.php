5a489ca59cc41698483207448f75ebedcb6f1a59
dawiteu <dawid@tararuj.be>
2021-05-11 10:51:22 +0200

    exo

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,11 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav w-100">
+            @yield('navcontent')
+        </ul>
+        </div>
+    </nav>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.nav')
+
+@section('navcontent')
+    <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Equipes</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Joueurs</a>
+    </li>
+    <li class="nav-item ml-auto">
+        <a class="nav-link" href="#">Admin</a>
+    </li>   
+@endsection
